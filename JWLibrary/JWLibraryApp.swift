d70833ffc561b68625b901b6787961c84bd17722
Dario Ragusa <46404000+Miaosi001@users.noreply.github.com>
2021-01-01 13:01:13 +0100

    Initial Commit

---
+++ JWLibrary/JWLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  JWLibraryApp.swift
+//  JWLibrary
+//
+//  Created by Dario Ragusa on 01/01/21.
+//
+
+import SwiftUI
+
+@main
+struct JWLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
